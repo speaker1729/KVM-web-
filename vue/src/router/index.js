f04e58966f7f1f69108e3df366db1ff4ac777383
speaker1729 <126320604+speaker1729@users.noreply.github.com>
2024-07-28 12:22:45 +0800

    Add files via upload

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,1 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home',
+    component: () => import('../components/HomeView.vue'),
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('../views/home.vue'),
+      },
+    ],
+  },
+];
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
