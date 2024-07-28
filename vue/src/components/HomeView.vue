bc1238baadf53275c159591018d818ebf5700c13
speaker1729 <126320604+speaker1729@users.noreply.github.com>
2024-07-28 12:17:45 +0800

    Add files via upload

---
+++ vue/src/components/HomeView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <el-menu
+        default-active="1-4-1"
+        class="el-menu-vertical-demo"
+        background-color="#333"
+    >
+      <router-link :to="{ name: 'home' }">
+        <el-menu-item index="1" class="menu-item-center">
+          <i class="el-icon-s-home"></i>
+        </el-menu-item>
+      </router-link>
+    </el-menu>
+    <div class="center">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style>
+.home {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  overflow: hidden;
+}
+
+.center {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 95vw;
+  height: 100%;
+}
+
+.el-menu-vertical-demo {
+  box-sizing: border-box;
+  width: 5vw;
+  height: 100vh;
+  z-index: 9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.menu-item-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.el-menu-vertical-demo .el-menu-item.is-active {
+  background-color: #000000;
+}
+
+.el-menu-item i {
+  color: #FFF;
+  font-size: 56px;
+  font-weight: bold;
+}
+</style>
